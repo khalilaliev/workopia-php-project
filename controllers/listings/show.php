9f755498537d699de3a5261d1a3091934ef5af73
khalilaliev <khalilaliev0@gmail.com>
2025-05-24 20:43:34 +0200

    add listing details

---
+++ controllers/listings/show.php
@@ -0,0 +1,17 @@
+<?php
+
+$config = require base_path('config/db.php');
+$db = new Database($config);
+
+$id = $_GET['id'] ?? '';
+
+$params = [
+  'id' => $id
+];
+
+$listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+
+load_view('listings/show', [
+  'listing' => $listing,
+]);
--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require base_path('Router.php');
 $router = new Router();
 $routes = require base_path('routes.php');
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
