199a7e7013ad17bd5180e566167b62d97e996040
khalilaliev <khalilaliev0@gmail.com>
2025-08-07 11:11:00 +0200

    add logout & clear session

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -135,4 +135,21 @@ class UserController
 
     redirect('/');
   }
+
+  /**
+   * Logout user kill session
+   * 
+   * @return void
+   */
+
+  public function logout()
+  {
+    Session::clear_all('user');
+
+    $params = session_get_cookie_params();
+
+    setcookie('PHPSESSID', '', time() - 86400, $params['path'], $params['domain']);
+
+    redirect('/');
+  }
 }
